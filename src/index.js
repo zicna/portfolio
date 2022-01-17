f88ed1b9762f8ac6bf048b39982a4c379e0c4be4
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-01-17 16:08:05 -0600

    basic js done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 const hambugerButton = document.getElementById("hamburger")
+const navList = document.getElementById("nav-list")
+
+const toggleButton = () => {
+    navList.classList.toggle("show")
+}
+
+hambugerButton.addEventListener("click", toggleButton)
