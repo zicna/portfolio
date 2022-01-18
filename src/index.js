d44ae481dcb9fd23eb1c1afd722e4bfaae3294ab
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-01-18 12:16:03 -0600

    add smooth scrolling efect

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-const hambugerButton = document.getElementById("hamburger")
-const navList = document.getElementById("nav-list")
+const hambugerButton = document.getElementById('hamburger')
+const navList = document.getElementById('nav-list')
 
 const toggleButton = () => {
-    navList.classList.toggle("show")
+  navList.classList.toggle('show')
 }
 
-hambugerButton.addEventListener("click", toggleButton)
+hambugerButton.addEventListener('click', toggleButton)
+
+// * Adding smooth scrolling when a tag is clicked
+document.querySelectorAll('a[href^="#"]').forEach((a) => {
+  a.addEventListener('click', function (e){
+    e.preventDefault()
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
+    })
+  })
+})
+
